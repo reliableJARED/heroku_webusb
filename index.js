6075ef58eceb0b9568be0364e6694854d4ec33b3
jared <Jared>
2020-11-29 16:46:01 -0500

    got one way streaming to work, need to set the response answer to remoteVideo element

--- index.js
+++ index.js
@@ -20,40 +20,43 @@ const http = require('http').Server(app);
 //A server that integrates with (or mounts on) the Node.JS HTTP Server: socket.io
 const io = require('socket.io')(http);
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 1;
+
+const roomPasswordLocker =( ()=>{
+  //key:value where key=room name, value=room password
+  const room_pw = {};
+  
+  return (room,pass)=>{
+    room = room || false;
+  	pass = pass || false;
+    if(room && pass){
+       //SET PW - if room and password arg sent
+        console.log('setting room: '+room+" to pw: "+pass);
+        room_pw[room]=pass;
+        }
+    return room_pw;
+    };
+    
+})();
+  
+  
+function set_roomPasswordLocker(room,pw){
+  console.log('try to set room: '+room+" to pw: "+pw);
+  roomPasswordLocker(room,pw);
+}
+
+function getPassword_roomPasswordLocker(room){
+  allPWs = roomPasswordLocker();
+  console.log('all PWs: '+allPWs);
+  roomPW = allPWs[room];
+  console.log('accessed password for room: '+room);
+  console.log('current pw is:' + roomPW);
+  return roomPW;
+}
+
 
 //cheat sheet
 //https://socket.io/docs/v3/emit-cheatsheet/index.html
-
-function roomIDlocker(id,room){
-  const ids_room = {};
-  if(id && room){
-  (id,room)=>{ids_room[id]=room;};
-  }
-  else if(id && room === 'undefined'){
-    delete ids_room[id];
-  }
-  return ids_room;
-}
-
-function get_roomIDlocker(room){
- const allclients = roomIDlocker();
- const roomclients = Object.keys(allclients).find(key => allclients[key] === room);
- //below is faster?
- //const roomclients = Object.keys(allclients).filter(key => allclients[key] === room);
- 
- return roomclients;
-}
-
-function add_roomIDlocker(id,room){
- roomIDlocker(id,room);
-}
-
-function remove_roomIDlocker(id){
-  roomIDlocker(id);
-}
-
-//remove the 'receiver concept'
 io.sockets.on("connection", socket => {
   
 
@@ -62,105 +65,150 @@ io.sockets.on("connection", socket => {
     socket.emit('log', stringMsg);
   }
   
-  socket.on('message', function(room,message) {
+  socket.on('message', (room,message)=> {
     log('Client said: '+ message);
     io.to(room).emit('message', message,socket.id);
   });
 
-  socket.on('create or join', function(room) {
+
+function createNewRoom(soc,room,password){
+  console.log("createNewRoom() socket with ID: "+soc.id);
+  console.log("createNewRoom() "+room+" _ "+password);
+}
+
+function joinRoom(soc,room,password){
+  console.log("joinRoom() socket with ID: "+soc.id);
+  console.log("joinRoom() "+room+" _ "+password);
+}
+
+socket.on('create or join', (room,password) =>{
+    //MAXIMUM Room Members
+    const maxRoomMembers = 2;
+    
+    //eventually use ONLY auto-generated room names.  Would also have to check against all current room names.
+    //something like randomMath or better.  Socket.io may already have function i just don't know atm
+    room = room || 'room_'+Math.floor(Math.random() * Math.floor(1000));
+    
+    /*SECURITY
+      'create or join' should handle a password
+      room creator sets, joiner has a check.
+    */
+    password = password || '1234'; //evenutally use pw from connecting client
+    
+    //This isn't used, but at some point need to check if user supplied room name exists
+    //OR just use auto generated room names, prob a better solution :)
+    const AllRoomNames = io.sockets.adapter.rooms;
     
     //console.log('Received request to create or join room '+room);
     log('Received request to create or join room ' + room);
     
-    let clientsInRoom;
+    //first check if room already exists, if no room then it has 0 clients
+    let clientsInRoom = io.sockets.adapter.rooms.get(room) ? io.sockets.adapter.rooms.get(room).size : 0;
+    let isInitiatorClient;//flag to see if first attendee
     
     //first check if room already exists
-    if(!io.sockets.adapter.rooms.get(room)){
-      log('making room ' +room);
-      clientsInRoom = 0;
-    }
-    else{
-      //if room exists, find out how many people are in it
+    if(clientsInRoom === 0){
+      //PLACE HOLDER - eventually move this workflow to function
+      createNewRoom(socket,room,password);
+      
+      //set room password
+      set_roomPasswordLocker(room,password);
+      
+      log('room '+room+' not found, making room ' +room);
+      //add client to the new room
+      socket.join(room);
+      //update room member count after join
       clientsInRoom = io.sockets.adapter.rooms.get(room).size;
-      console.log(io.sockets.adapter.rooms.get(room))
-      console.log('room ' +room+' already exists');
-      log('Room ' + room + 'currently has ' + clientsInRoom + ' client(s)');
+      //flag client as first attendee in room
+      isInitiatorClient = true;
+      //tell cleint room joined
+      socket.emit('joined',room,socket.id,isInitiatorClient);
+      //debug logs
+      log('Client ID ' + socket.id + ' created room ' + room);
+      log('Room: ' + room + ' now has ' + clientsInRoom + ' client(s)');
+    }
+    else if(getPassword_roomPasswordLocker(room) === password){
+      console.log('correct password for '+ room+ ' supplied');
+      log('correct password for '+ room+ ' supplied');
+      log('Room ' + room + ' currently has ' + clientsInRoom + ' client(s)');
+      
+       //if room exists, find out if it's too full to join
+      if (clientsInRoom < maxRoomMembers) {
+        //PLACE HOLDER - eventually move this workflow to function
+        joinRoom(socket,room,password);
+      
+        //join room
+        socket.join(room);
+        //update room member count after join
+        clientsInRoom = io.sockets.adapter.rooms.get(room).size;
+        //flag client as NOT first attendee in room
+        isInitiatorClient = false;
+        //debug log
+        log('Client ID ' + socket.id + ' joined room ' + room);
+        
+        //tell cleint, they joined
+        socket.emit('joined', room, socket.id,isInitiatorClient);
+        
+        //DROP THIS - newMember is only one who knows who is in room, starts the connection handshake
+        //tell all clients, except sender, a new member joinded the room
+        //socket.to(room).emit('newRoomMember',room,socket.id);
+        
+        //get all room members
+        let allRoomMembers = io.sockets.adapter.rooms.get(room);
+        //remove self from Set
+        allRoomMembers.delete(socket.id);
+        //convert Set object to Array
+        let allRoomMembers_array = Array.from(allRoomMembers);
+        //tell sender who is in the room, excluding self
+        socket.emit('newRoomMember',room,allRoomMembers_array);
+
+        //debug log
+        log('Room ' + room + ' now has ' + clientsInRoom + ' client(s)');
+      }
+      else{ // maximum occupancy
+        socket.emit('full', room);
+      }
     }
     
-
-    if (clientsInRoom === 0) {
-      socket.join(room);
-      add_roomIDlocker(socket.id,room);
-      
-      clientsInRoom = io.sockets.adapter.rooms.get(room).size;
-      //console.log(io.sockets.adapter.rooms);
-      //console.log(typeof(io.sockets.adapter.rooms.get(room)));
-      socket.emit('created',room,socket.id);
-      //io.to(room).emit('created', room, socket.id);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      log('Room ' + room + ' now has ' + clientsInRoom + ' client(s)');
-      //socket.emit('created', room, socket.id);
-      //clientsInRoom = io.sockets.adapter.rooms[room];
-      //console.log("room clients: "+clientsInRoom);
-
-    }
-    else if (clientsInRoom === 1) {
-      socket.join(room);
-      add_roomIDlocker(socket.id,room);
-      
-      clientsInRoom = io.sockets.adapter.rooms.get(room).size;
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      //tell cleint, they joined
-      socket.emit('joined', room, socket.id);
-      //tell all clients, except sender, a new member joinded the room
-      socket.to(room).emit('newRoomMember',room,socket.id);
-      //tell all clients in room, room is ready
-      io.sockets.in(room).emit('ready',room);
-      log('Room ' + room + ' now has ' + clientsInRoom + ' client(s)');
-    }
-    else { // max two clients
-      socket.emit('full', room);
-    }
   });
 
 
-//TODO - find a way to get all members of a room
+//This is probably going to be dropped, each time someone connects, they reach out to the other clients
+//may not be a need to 'ask' for other connected members
 socket.on('askForOtherRoomMembers',room=>{
-  //let allRoomMembers = get_roomIDlocker(room);
+  //get all room members
   let allRoomMembers = io.sockets.adapter.rooms.get(room);
   //remove self from list
   allRoomMembers.delete(socket.id);
-  let scroll = allRoomMembers.values();
-
-    console.log('client: '+scroll[0]);
+  //convert Set object to Array
+  let allRoomMembers_array = Array.from(allRoomMembers);
+  console.log('client: '+ allRoomMembers_array);
   
-  //eventually change this to return a list of ALL socketIDs in the room
-  //var AllClients = Object.keys(io.sockets.in(room).connected)
-  console.log('all clients in room: '+allRoomMembers.entries());
-  log('all clients in room: '+ allRoomMembers);
-  socket.emit('newRoomMember',room,allRoomMembers);
+  //return a list of ALL socketIDs in the room
+  log('all clients in room: '+ allRoomMembers_array);
+  socket.emit('newRoomMember',room,allRoomMembers_array);
 });
 
-  socket.on('offer',(id,offer)=>{
+socket.on('offer',(id,offer)=>{
     //relay offer from Alice to Bob
-    console.log(id+" sent offer");
-    log('client ' + socket.id + ' sent an offer to ' + id);
+    console.log(socket.id+" sent offer "+offer);
+    log('client ' + socket.id + ' sent an offer '+ offer +' to ' + id);
     socket.to(id).emit("offer", socket.id, offer);
   });
   
-  socket.on('answer',(id,answer)=>{
+socket.on('answer',(id,answer)=>{
     //relay response from Bob to Alice
-     console.log(id+" sent answer");
+    console.log(socket.id+" sent answer to "+id);
+    socket.to(id).emit("answer", socket.id, answer);
   });
   
- socket.on('candidate',(id,candidate)=>{
-    //relay response from Bob to Alice
-     console.log(id+" sent candidate");
-    
+socket.on('candidate',(id,candidate)=>{
+    //relay response
+     console.log(socket.id+" sent candidate to: "+id);
+     socket.to(id).emit("candidate", socket.id, candidate);
   });
   
   socket.on('bye', room=>{
-    remove_roomIDlocker(socket.id,room);
     console.log('received bye from '+socket.id);
   });
   
--- public/js/client_RTCPeerConnection.js
+++ public/js/client_RTCPeerConnection.js
@@ -22,7 +22,6 @@ https://stackoverflow.com/questions/57181851/how-to-make-webrtc-application-work
 
 'use strict';
 
-
 /////////////////////////////////////////////
 
 // Could prompt for room name:
@@ -61,7 +60,7 @@ var localSocketID;
 var isInitiator = false; //flag to determine who starts RTC connection handshake
 var LocalMediaStreamReady = false; //flag if media is ready
 //////////////////////GET MEDIA ELEMENTS////////////////////////
-var remoteVideo = document.querySelector('#remoteVideo');
+const remoteVideo = document.querySelector('#remoteVideo');
 
 /* PREPARE localPeerConnection */
 const constraints = {
@@ -70,6 +69,7 @@ const constraints = {
     };
 //FIRST - get local media stream
 const localStream = navigator.mediaDevices.getUserMedia(constraints);
+
 localStream.then(
   //SECOND - get the media stream tracks and add to the localPeerConnection, return stream for next .then()
   (stream)=>{
@@ -90,23 +90,36 @@ localStream.then(
   .then(
   //FIFTH - indicate local media stream is ready;
     ()=>{LocalMediaStreamReady = true}
+  ).then(
+  //SIXTH - Send msg to server to join a room
+  joinARoom
   );
   
 function setLocalVideo(stream){
   let localVideo = document.querySelector('#localVideo');
-  localVideo.srcObject = stream
-};
-
-function getLocalVideo_fromHTML(){
-  return document.querySelector('#localVideo').srcObject;
+  localVideo.srcObject = stream;
 }
+
+
 //Done preparing local peerconnection for now
 
+function joinARoom(){
+  //response back if client joined an existing room called 'chatRoom'
+  socket.on('joined',  (room, socketID,isInitiatorClient)=> {
+  localSocketID = socketID;
+  isInitiator = isInitiatorClient;//if Flase, then start handshake.  Means others are in the room
+  console.log('Client joined '+room+' Client socketID is: ' + socketID+' Client created room: '+isInitiator);
+  
+  //need to ask for the other room members ID if you didn't create the room
+  /*if(!isInitiator){
+    socket.emit('askForOtherRoomMembers', room);
+  }*/
+  });
+}
 
 
 
-
-console.log(localPeerConnection)
+console.log(localPeerConnection);
 
 /*********** SOCKET FUNCTIONS
  */
@@ -115,69 +128,108 @@ socket.on('connect',(msg)=>{
 	    console.log("CONNECTED");
 			
       //room is hardcoded atm, so this should always run
+      //TODO - add a user input for room to join existing room
       if (room !== '') {
         socket.emit('create or join', room);
         console.log('Asking server to create or join room: ', room);
-        };
+        }
 		});
 
-//response back if client created room called 'chatRoom'
-socket.on('created', (room,socketID)=> {
-  localSocketID = socketID;
-  console.log('Created room ' + room + ' ,your socketID is: '+socketID);
-  isInitiator = true;
-});
-
-//response back if client joined an existing room called 'chatRoom'
-socket.on('joined',  (room, socketID)=> {
-  localSocketID = socketID;
-  console.log('Client joined '+room+' your socketID is: ' + socketID);
-  isInitiator = false;
-  //need to ask for the other room members ID
-  socket.emit('askForOtherRoomMembers', room);
-});
-
-//response back if a new client joined the room OR if 'askForOtherRoomMember' is sent.
+//response back if Client joined a room with other clients
 socket.on("newRoomMember", (room,id) => {
-  console.log('new member in room')
-  console.log(id.entries());
-  //TODO - Eventually this will receive a JSON of ALL members in the room
-  //will need to loop this next section to create a RTCpc for each.
+  //id an array of socket.IDs of other members
+  console.log(id.length+' other members in room: '+room);
+  console.log(id[0]);
+  
+ // change '0' holder in id[0] when for loop completed
+  
+  //TODO - Eventually this will loop the Array of ALL member Socket.IDs in the room
+  //will need to create a RTCpc for each.
   const remotePeerConnection = new RTCPeerConnection(iceConfig);
-  allPeerConnections[id] = remotePeerConnection;
+  
+  allPeerConnections[id[0]] = remotePeerConnection;
 
- //add the local media tracks to the RTCpc
- let stream = getLocalVideo_fromHTML();
- stream.getTracks().forEach(track => remotePeerConnection.addTrack(track, stream));
+ //IMPORTANT - everything has to wait for userMedia, so it's all chained to that promise .then()
+ //https://stackoverflow.com/questions/38036552/rtcpeerconnection-onicecandidate-not-fire
+ navigator.mediaDevices.getUserMedia(constraints)
+ .then(
+   (stream)=>{
+     stream.getTracks().forEach(track => remotePeerConnection.addTrack(track, stream));
+     return remotePeerConnection
+   })
+ .then((rpc) =>{
+   rpc.createOffer()
+    .then(sdp => {
+      rpc.setLocalDescription(sdp);
+      //there was an issue with the set completion and the emit, bubble the sdp through .then()
+      return sdp;
+    })
+    .then((sdp) => {
+        console.log('sending offer to: '+id[0]);
+        console.log('localDescription: ');
+        console.log(sdp);
+        socket.emit("offer", id[0], sdp);
+      })
+    });
 
   //create handler for the .onicecandidate method of RTCPeerConnection instance localPeerConnection
-  localPeerConnection.onicecandidate = event => {
+  remotePeerConnection.onicecandidate = event => {
         if (event.candidate) {
-          socket.emit("candidate", id, event.candidate);
+          console.log('event candidate received, send back to id: '+id[0]);
+          socket.emit("candidate", id[0], event.candidate);
         }
-      }
+      };
+});
 
-  remotePeerConnection
-    .createOffer()
-    .then(
-      sdp => remotePeerConnection.setLocalDescription(sdp)
-      )
-    .then(
-      () => {socket.emit("offer", id, remotePeerConnection.localDescription);}
-      );
+socket.on("offer", (id, description) => {
+  console.log('offer: '+description+' from: '+id);
+  const remotePeerConnection = new RTCPeerConnection(iceConfig);
+  
+  allPeerConnections[id] = remotePeerConnection;
+  
+  remotePeerConnection.setRemoteDescription(description)
+    .then(() => remotePeerConnection.createAnswer())
+    .then(sdp => remotePeerConnection.setLocalDescription(sdp))
+    .then(() => {
+      console.log('sending answer to id: '+id);
+      socket.emit("answer", id, remotePeerConnection.localDescription);
+    });
+  remotePeerConnection.ontrack = event => {
+    console.log('event');
+    console.log(event);
+    remoteVideo.srcObject = event.streams[0];
+  };
+  remotePeerConnection.onicecandidate = event => {
+    console.log('received event');
+    console.log(event);
+    if (event.candidate) {
+      socket.emit("candidate", id, event.candidate);
+    }
+  };
 });
 
 socket.on("answer", (id, description) => {
+  /***** FIX THIS ASAP
+   * doesn't handle an answer, so the second connecting client can't see remote video
+   */
+  console.log('received answer from: '+id);
   allPeerConnections[id].setRemoteDescription(description);
+  allPeerConnections[id].ontrack = event => {
+    console.log('answer event');
+    console.log(event);
+    remoteVideo.srcObject = event.streams[0];
+  };
 });
 
+
 socket.on("candidate", (id, candidate) => {
+  console.log('received candidate from: '+id);
   allPeerConnections[id].addIceCandidate(new RTCIceCandidate(candidate));
 });
 
 socket.on("disconnectPeer", id => {
-  peerConnections[id].close();
-  delete peerConnections[id];
+  allPeerConnections[id].close();
+  delete allPeerConnections[id];
 });
 
 //debug helper
